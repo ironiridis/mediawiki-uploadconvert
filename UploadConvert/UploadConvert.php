d456c1731f48686e8affd215f57ba5ab4eb542c7
Christopher Harrington <ironiridis@gmail.com>
2012-08-25 21:06:22 -0500

    cleaner implementation
    It makes more sense to implement this as an extension of the existing
    upload functions and intercept their initialization function.

--- UploadConvert/UploadConvert.php
+++ UploadConvert/UploadConvert.php
@@ -30,84 +30,98 @@ $wgExtensionCredits['UploadConvert'][] = array(
         'version' => '0.0.0'
 );
 
-function extUploadConvertIntercept()
-{
+class extUploadConvertBase {
+	static protected $opt = array();
 	
-}
-
-function extUploadConvertByExtention($matchext, $newext, $cmd, $opt=array())
-{
-	if (!(is_string($matchext) and is_string($newext) and is_string($cmd)))
-		throw new Exception('All arguments for filter must be strings');
-	
-	if (!is_array($opt)) $opt = array($opt);
-	
-	while(strlen($matchext) > 0 and $matchext[0] == '.')
-		$matchext = substr($matchext,1); // strip leading dots
-	if ($matchext == '') throw new Exception('Invalid matching file extention');
-	
-	if ($newext == '') // keep file extension despite being converted
-		$newext = $matchext;
-	else
+	static protected function getOptions()
 	{
-		while(strlen($newext) > 0 and $newext[0] == '.')
-			$newext = substr($newext,1); // strip leading dots
-		if ($newext == '') throw new Exception('Invalid new file extention');
-	}	
-	
-	// return values don't work consistently with windows apps
-	if (strpos(php_uname('s'), 'Windows') !== false)
-		$opt[] = 'ignore_return_value';
-	
-	return(array(
-		'matchType'=>'extension',
-		'extension'=>$matchext,
-		'newextension'=>$newext,
-		'command'=>$cmd,
-		'options'=>$opt
-	));
-}
-
-function extUploadConvertExecute($file, $newext, $cmd, &$opt)
-{
-	$pi = @pathinfo($file);
-	$newfn = $file.'.'.$newext;
-	$cmd = str_replace('%from%', escapeshellarg($file), $cmd);
-	$cmd = str_replace('%fromext%', escapeshellarg($pi['extension']), $cmd);
-	$cmd = str_replace('%frombase%', escapeshellarg($pi['basename']), $cmd);
-	$cmd = str_replace('%dir%', escapeshellarg($pi['dirname']), $cmd);
-	$cmd = str_replace('%to%', escapeshellarg($newfn), $cmd);
-	$cmd = str_replace('%toext%', escapeshellarg($newext), $cmd);
-	
-	if (in_array('allow_raw_args', $opt))
-	{
-		$cmd = str_replace('%raw-from%', $file, $cmd);
-		$cmd = str_replace('%raw-fromext%', $pi['extension'], $cmd);
-		$cmd = str_replace('%raw-frombase%', $pi['basename'], $cmd);
-		$cmd = str_replace('%raw-dir%', $pi['dirname'], $cmd);
-		$cmd = str_replace('%raw-to%', $newfn, $cmd);
-		$cmd = str_replace('%raw-toext%', $newext, $cmd);
+		return(self::$opt);
 	}
 	
-	$r = 255; // return value from the command
-	exec($cmd, null, $r);
-	if (in_array('ignore_return_value', $opt))
-		$r = 0; // fake success
-	
-	if (!file_exists($newfn)) return false;
-	if ($r != 0) // command failed
+	static public function filterByExtention($matchext, $newext, $cmd, $opt=array())
 	{
-		@unlink($newfn); // remove new file
-		return false;
+		if (!(is_string($matchext) and is_string($newext) and is_string($cmd)))
+			throw new Exception('First 3 arguments for filter must be strings');
+		
+		if (!is_array($opt)) $opt = array($opt);
+		
+		while(strlen($matchext) > 0 and $matchext[0] == '.')
+			$matchext = substr($matchext,1); // strip leading dots
+		if ($matchext == '') throw new Exception('Invalid matching file extention');
+		
+		if ($newext == '') // keep file extension despite being converted
+			$newext = $matchext;
+		else
+		{
+			while(strlen($newext) > 0 and $newext[0] == '.')
+				$newext = substr($newext,1); // strip leading dots
+			if ($newext == '') throw new Exception('Invalid new file extention');
+		}	
+		
+		// return values don't work consistently with windows apps
+		if (strpos(php_uname('s'), 'Windows') !== false)
+			$opt[] = 'ignore_return_value';
+		
+		self::$opt[] = array(
+			'matchType'=>'extension',
+			'extension'=>$matchext,
+			'newextension'=>$newext,
+			'command'=>$cmd,
+			'options'=>$opt
+		);
 	}
 	
+	protected function extUploadConvertExecute($file, $newext, $cmd, &$opt)
+	{
+		$pi = @pathinfo($file);
+		$newfn = $file.'.'.$newext;
+		$cmd = str_replace('%from%', escapeshellarg($file), $cmd);
+		$cmd = str_replace('%dir%', escapeshellarg($pi['dirname']), $cmd);
+		$cmd = str_replace('%to%', escapeshellarg($newfn), $cmd);
+		$cmd = str_replace('%toext%', escapeshellarg($newext), $cmd);
+		
+		if (in_array('allow_raw_args', $opt))
+		{
+			$cmd = str_replace('%raw-from%', $file, $cmd);
+			$cmd = str_replace('%raw-dir%', $pi['dirname'], $cmd);
+			$cmd = str_replace('%raw-to%', $newfn, $cmd);
+			$cmd = str_replace('%raw-toext%', $newext, $cmd);
+		}
+		
+		$r = 255; // return value from the command
+		exec($cmd, $discard = array(), $r); unset($discard);
+		if (in_array('ignore_return_value', $opt))
+			$r = 0; // fake success
+		
+		if (!file_exists($newfn)) return false;
+		if ($r != 0) // command failed
+		{
+			@unlink($newfn); // remove new file
+			return false;
+		}
+		
+		return true;
+	}
+}
+
+class extUploadConvertFile extends UploadFromFile {
+	/* ** TODO ** */
+}
+
+class extUploadConvertStash extends UploadFromStash {
+	/* ** TODO ** */
+}
+
+class extUploadConvertUrl extends UploadFromUrl {
+	/* ** TODO ** */
+}
+
+function extUploadConvertIntercept($type, $className)
+{
+	$n = 'extUploadConvert'.$type;
+	if (class_exists($n)) $className = $n;
+
 	return true;
 }
 
-$extUploadConvertSettings = array();
-
-$wgHooks['UploadCreateFromRequest'][]=array(
-	'extUploadConvertIntercept',
-	$extUploadConvertSettings
-);
-
+$wgHooks['UploadCreateFromRequest'][]='extUploadConvertIntercept';
